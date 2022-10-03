1ef7b1e9896b086926583085af7201e0aaf6a225
fran <ctefran@hotmail.com>
2022-10-03 15:15:23 +0200

    Dom

--- xmljson/src/main/java/com/fran/xmljson/utilidades/XmlUtils.java
+++ xmljson/src/main/java/com/fran/xmljson/utilidades/XmlUtils.java
@@ -1,11 +1,18 @@
 package com.fran.xmljson.utilidades;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -73,4 +80,39 @@ public class XmlUtils {
 		}
 		return null;
 	}
+	
+	public static void procesarXmlDom(String rutaCompleta) {
+		try {
+			File inputFile = new File(rutaCompleta);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+			System.out.println("Elemento base : " + doc.getDocumentElement().getNodeName());
+			NodeList nList = doc.getElementsByTagName("asignatura");
+			System.out.println();
+			System.out.println("Recorriendo asignaturas...");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					System.out.println("Codigo: " + eElement.getAttribute("id"));
+					System.out.println("Nombre: " + eElement.getElementsByTagName("nombre").item(0).getTextContent());
+					System.out.println(
+							"Ciclo: " + eElement.getElementsByTagName("cicloFormativo").item(0).getTextContent());
+					System.out.println("Curso: " + eElement.getElementsByTagName("curso").item(0).getTextContent());
+					System.out
+							.println("Profesor: " + eElement.getElementsByTagName("profesor").item(0).getTextContent());
+					System.out.println();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	
+	
 }
