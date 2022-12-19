b8ac3deefc6c0c3eeae87cb8a849feebe138ac90
fran <ctefran@hotmail.com>
2022-12-19 03:21:41 +0100

    Práctica 4 esqueleto

---
+++ Hibernate/practica4/src/main/java/com/dam/practica4/App.java
@@ -0,0 +1,13 @@
+package com.dam.practica4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
