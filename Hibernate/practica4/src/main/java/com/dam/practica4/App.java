38496c597de568c639f26973331be47925decbc1
fran <ctefran@hotmail.com>
2022-12-20 20:26:45 +0100

    Esqueleto muy mejorado

--- Hibernate/practica4/src/main/java/com/dam/practica4/App.java
+++ Hibernate/practica4/src/main/java/com/dam/practica4/App.java
@@ -1,13 +1,35 @@
 package com.dam.practica4;
 
-/**
- * Hello world!
- *
- */
+import com.dam.practica4.entidades.Categories;
+import com.dam.practica4.utilidades.HibernateUtils;
+
 public class App 
 {
+	
+	public static void probarConexion() {
+		if(HibernateUtils.abrirConexion()) {
+			System.out.println("Conexión establecida correctamente");
+			HibernateUtils.cerrarConexion();
+		} else {
+			System.out.println("Fallo al establecer la conexión");
+		}
+	}
+	
+	/**
+	 * Ejemplo de como acceder a la lista de elementos de una clase
+	 * En el ejemplo categories y recorrerla
+	 */
+	public static void listarCategorias() {
+		HibernateUtils.abrirConexion();
+		HibernateUtils.devolverListaObjetos(Categories.class)
+			.forEach(c->System.out.println(c));
+		HibernateUtils.cerrarConexion();
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.SEVERE);
+    	//probarConexion();
+    	listarCategorias();
     }
 }
