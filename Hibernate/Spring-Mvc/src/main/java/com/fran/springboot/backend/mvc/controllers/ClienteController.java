07ac5858df5744752749715b8128101e9d98d1b6
fran <ctefran@hotmail.com>
2023-01-27 15:46:37 +0100

    POST

--- Hibernate/Spring-Mvc/src/main/java/com/fran/springboot/backend/mvc/controllers/ClienteController.java
+++ Hibernate/Spring-Mvc/src/main/java/com/fran/springboot/backend/mvc/controllers/ClienteController.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -82,12 +84,45 @@ public class ClienteController {
 		return new ResponseEntity<Cliente>(cliente,HttpStatus.OK);
 	}
 	
-	@PostMapping("")
+	/*@PostMapping("")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente insertar(@RequestBody Cliente cliente) {
 		cliente.setCreateAt(LocalDate.now());
 		return clienteService.save(cliente);
+	}*/
+	
+	@PostMapping("")
+	public ResponseEntity<?> create(@RequestBody Cliente cliente, BindingResult result){
+		
+		Cliente clienteNew = null;
+		Map<String,Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo '" + err.getField() +"' "+ err.getDefaultMessage())
+					.collect(Collectors.toList());
+			
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+		
+		try {
+			cliente.setCreateAt(LocalDate.now());  // Cambia la fecha a la actual
+			clienteNew = clienteService.save(cliente);
+		} catch (DataAccessException e) {  // Error al acceder a la base de datos
+			response.put("mensaje", "Error al conectar con la base de datos");
+			response.put("error", e.getMessage().concat(":")
+					.concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String,Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El cliente se ha insertado correctamente");
+		response.put("cliente", clienteNew);
+		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.CREATED);
 	}
+	
 	
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
